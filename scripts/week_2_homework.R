a50ed2c232017501c7bc0e89ffbe03caf2c543cd
Land-In-Thunder <ltnoland@ucdavis.edu>
2021-10-04 15:48:22 -0700

    hw2 complete

---
+++ scripts/week_2_homework.R
@@ -0,0 +1,19 @@
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+
+hw2 <- na.omit(hw2)
+
+hw2
+
+prob1 <- hw2[hw2 >= 14 & hw2 <= 38]
+prob1
+
+times3 <- 3 * prob1
+times3
+
+plus10 <- 10 + times3
+plus10
+
+plus10[c(T,F)]
