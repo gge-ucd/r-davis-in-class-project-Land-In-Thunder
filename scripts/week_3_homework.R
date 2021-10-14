45e5e7a9ebde8c3051b4487b96a32f9667185d7f
Land-In-Thunder <ltnoland@ucdavis.edu>
2021-10-14 15:02:10 -0700

    corrected na.omit

--- scripts/week_3_homework.R
+++ scripts/week_3_homework.R
@@ -6,7 +6,7 @@ speciesID_factor <- as.factor(surveys_base[,c(1)])
 speciesID_factor
 plot_type_factor <- as.factor(surveys_base[,c(3)])
 plot_type_factor
-surveys_base_naomit <- na.omit(surveys_base)
+surveys_base_naomit <- surveys_base %>% na.omit(surveys_base$weight)
 surveys_base_naomit
 
 surveys_new <- surveys_base_naomit[(surveys_base_naomit[,2]>150),]
